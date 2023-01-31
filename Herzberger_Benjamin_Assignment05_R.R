66afafb640104e443db823b9b6705cb21376ffae
Benjamin Herzberger <benjamin.herzberger@student.uni-tuebingen.de>
2023-01-31 11:55:08 +0100

    Saving what I have so far

--- Herzberger_Benjamin_Assignment05_R.R
+++ Herzberger_Benjamin_Assignment05_R.R
@@ -44,36 +44,82 @@ df <- data.frame(
   
 
 #Really ugly solution
+#Event name
 for(i in 1:20){
   df[i, 1] <- temp[[1]][[1]][[i]][[1]]
 }
 
+#City name
 for(i in 1:20){
   df[i, 2] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["city"]][["name"]]
 }
 
+#Postal Code
 for(i in 1:20){
   df[i,3] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["postalCode"]]
 }
 
+#Address
 for(i in 1:20){
   df[i,4] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["address"]][["line1"]]
 }
 
+#URL
 for(i in 1:20){
   df[i,5] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["url"]]
 }
 
+#Longitude
 for(i in 1:20){
   df[i,6] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["location"]][["longitude"]]
 }
 
+#Latitude
 for(i in 1:20){
   df[i,7] <- temp[["_embedded"]][["events"]][[i]][["_embedded"]][["venues"]][[1]][["location"]][["latitude"]]
 }
 
 str(df) #as instructed
 
+
 # Task 9: Advanced API interaction
+#number of results is 1410
+n_pages <- 1410/20
+temp2 <- list()
+for(i in 1:71){
+  Sys.sleep(.5)
+  venues <- GET("https://app.ticketmaster.com/discovery/v2/events/",
+                    query = list(apikey = api_key,
+                                 countryCode = "DE",
+                                 page   = i))
+
+#Extract the items
+temp2[i] <- content(venues)
+}
 
 
+#Setting up an empty df with specified dimensions
+n_results <- 1410
+
+df2 <- data.frame(
+  name = character(n_results),
+  city = character(n_results),
+  postalCode = character(n_results),
+  address = character(n_results),
+  url = character(n_results),
+  longitude = character(n_results),
+  latitude = character(n_results))
+ 
+#Filling the df as above
+#Event name
+for(i in 1:20){
+  df2[i, 1] <- temp2[[i]][["events"]][[i]][["name"]]
+}
+
+##I am very sorry, but an assignment in another module took up all my time.
+##I planned on uploading what I have so far, but now  RMarkdown cannot format 
+##the file somehow, even if I only try to Knit the standard RMarkdown default page.
+##Nevertheless I wanted to upload what I had so far, hoping it might be worth like half a point or something.
+
+# Github repository:
+#  
